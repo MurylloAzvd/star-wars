6bedab21b44bf5d31184447705de542382db8f29
Muryllo <murylloazvd@gmail.com>
2020-11-01 14:44:37 -0300

    Lista-Personagens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+// import Routes from './routes';
 
 function App() {
+
+  // const [page, setPage] = useState(1);
+  // const [isLoaded, setIsLoaded] = useState(false);
+
+  let url = "https://swapi.dev/api/people/";
+  const [pers, setPers] = useState([]);
+
+  // function inclementaPagina() {
+  //   setIsLoaded(false);
+  //   setPage(page + 1);
+
+  //   const urlPagination = url + "?page=" + page;
+
+
+  //   fetch(urlPagination)
+  //   .then(res => res.json())
+  //   .then((result) => {
+  //     setIsLoaded(true);
+  //     if (result.count > 0) {
+  //       setPers(result.results);
+  //     }
+  //   },
+  //   (error) => {
+  //     setIsLoaded(true);
+  //   })
+  // }
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then((result) => {
+        setPers(result.results);
+      })
+  }, []);
+
+  // fetch(url)
+  //   .then(res => res.json())
+  //   .then((result) => {
+  //     setPers(result.results);
+  //     console.log(pers);
+  //   })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+      <h1>Personagens</h1>
+      {console.log(pers)}
+      <ul>
+        {pers.map((p, index) => 
+          <li key={index}>{p.name}</li>
+        )}
+      </ul>
     </div>
   );
 }
